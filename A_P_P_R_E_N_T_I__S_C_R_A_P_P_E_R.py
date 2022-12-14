15ab7f9bb32670ecb5c34da12c01315cd9d3379d
adstuder42 <officialstunder@gmail.com>
2022-12-14 16:52:11 +0100

    maj

--- A_P_P_R_E_N_T_I__S_C_R_A_P_P_E_R.py
+++ A_P_P_R_E_N_T_I__S_C_R_A_P_P_E_R.py
@@ -5,7 +5,7 @@ import requests
 
 
 def get_question():
-    HOST = 'ctf06.root-me.org'
+    HOST = 'ctf19.root-me.org'
     PORT = 4444
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
@@ -15,72 +15,187 @@ def get_question():
     # print('no question :', datas)
     datas = client.recv(4096)
     print('Recu :', datas)
-    datastr = datas.decode('UTF-8')
-
+    datas = datas.decode('UTF-8')
 # url
+
+    print(datas)
+    split = datas.split("\"")
+    cookie = split[9]
+    url = split[3]
+    url = url.replace('XX', '19')
+    question = split[13]
+    question = question.replace('?', '')
+    question = question.replace("What\'s the ", "")
+    print(url)
+    print(question)
+    words = question.split(" ")
+    print(words)
+
+
+
+
+
+
+
+
+
+    
+    return 
     i = 9
     url = ""
-    while (datastr[i] != "\""):
-        if datastr[i] == 'X':
-            url = url + "06"
+    while (datas[i] != "\""):
+        if datas[i] == 'X':
+            url = url + "03"
             i += 2
         else:
-            url = url + datastr[i]
+            url = url + datas[i]
             i += 1
 
 # cookie
     cookie = ""
-    i = datastr.find("random")
+    i = datas.find("random")
     i += 10
-    while (datastr[i] != "\""):
-        cookie += datastr[i]
+    while (datas[i] != "\""):
+        cookie += datas[i]
         i += 1
+
+
+    datas = datas[datas.find("question"):]
+
+
+
+
+    first = dict(innerText = 0, innerHTML = 0, outerHTML = 0, lastChild = 0, firstChild = 0, parent = 0, random = 0, _class = 0, nonce = 0, footer = 0, td = 0, h3 = 0, tr = 0, id = 0, br = 0)
+
+
+
+
+    first = dict(innerText = 0, innerHTML = 0, outerHTML = 0, lastChild = 0, firstChild = 0, parent = 0, div = 0, i = 0)
+    second = dict(nonce = 0, id = 0, random = 0, lang = 0, _class = 0, tag = 0)
+    third = dict(ul = 0, strong = 0, footer = 0, p = 0, div = 0, h1 = 0)
+    
+    keywords = dict(of = 0)
+
+    of_first = dict(element = 0, a = 0, footer = 0, ul = 0, td = 0, li = 0, h1 = 0, br = 0, p = 0)
+    of_second = dict(footer = 0, ul = 0, strong = 0, div = 0)
+    # si of [1 2 3] IN 
+    if (datas.find("of ")) != -1:
+        keywords["of"] = 1
+ 
+ 
+ 
+    if datas[0:i].find("innerText") != -1:
+        first["innerText"] = 1
+    if datas[0:i].find("innerHTML") != -1:
+        first["innerHTML"] = 1
+    if datas[0:i].find("outerHTML") != -1:
+        first["outerHTML"] = 1
+    if datas[0:i].find("last child") != -1:
+        first["lastChild"] = 1
+    if datas[0:i].find("first child") != -1:
+        first["firstChild"] = 1
+    if datas[0:i].find("parent") != -1:
+        first["parent"] = 1
+    if datas[0:i].find("div") != -1:
+        first["div"] = 1
+    if datas[0:i].find("i's") != -1:
+        first["i"] = 1
+
+
+    if datas[0:i].find("nonce") != -1:
+        second["nonce"] = 1
+    if datas[0:i].find("id") != -1:
+        second["id"] = 1
+    if datas[0:i].find("random") != -1:
+        second["random"] = 1
+    if datas[0:i].find("lang") != -1:
+        second["lang"] = 1
+    if datas[0:i].find("class") != -1:
+        second["_class"] = 1
+
+
+
+    if datas[0:i].find("ul") != -1:
+        third["ul"] = 1
+    if datas[0:i].find("strong") != -1:
+        third["strong"] = 1
+    if datas[0:i].find("footer") != -1:
+        third["footer"] = 1
+    if datas[0:i].find("p") != -1:
+        third["p"] = 1    
+    if datas[0:i].find("div") != -1:
+        third["div"] = 1
+    if datas[0:i].find("h1") != -1:
+        third["h1"] = 1
+    if datas[0:i].find("h3") != -1:
+        third["h3"] = 1
+    if datas[0:i].find("h1") != -1:
+        third["h1"] = 1
+    if datas[0:i].find("h1") != -1:
+        third["h1"] = 1
+
+
+
+    print(first)
+    print(second)
+    print(third)
+    return 
 
 # identifier
     identifier = ""
-    i = datastr.find("with")
+    i = datas.find("with")
     if i != -1:
         i += 5
-        while (datastr[i] != "\""):
-            identifier += datastr[i]
+        while (datas[i] != "\""):
+            identifier += datas[i]
             i += 1
 
 # child
     child = ""
-    i = datastr.find("child")
+    i = datas.find("child")
     if i != -1:
-        if datastr.find("first") != -1:
+        if datas.find("first") != -1:
             child = "first"
-        elif datastr.find("last") != -1:
+        elif datas.find("last") != -1:
             child = "last"
+
 # parent
+    parent = ""
     if child == "":
         parent = "parent"
+
 # location
     location = ""
     if child == "":
-        i = datastr.find("s the")
+        i = datas.find("s the")
         if i != -1:
             i += 6
-            while (datastr[i] != ' '):
-                location += datastr[i]
+            while (datas[i] != ' '):
+                location += datas[i]
                 i += 1
-            
+
+# page_location
+    page_location = ""
+    if location == "page":
+        i = datas.find("page")
+        i += 5
+        while (datas[i] != '?'):
+            page_location += datas[i]
+            i += 1
+
+
 # target_type
     target_type = ""
     if child == "child":
-        i = datastr.find("child")
+        i = datas.find("child")
         i += 8
-
-
-
-
 
     print("url = " + url)
     print("cookie == " + cookie)
     print("child == " + child)
     print("parent == " + parent)
     print("location == " + location)
+    print("page_location == " + page_location)
     print("identifier == " + identifier)
 
     client.close()
